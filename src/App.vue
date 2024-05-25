92aff0a4381c76ae9afdf28de002a5bc6b78c4a4
areef44 <areef.empat.empat@gmail.com>
2024-05-25 10:32:07 +0700

    finish set map and set marker

--- src/App.vue
+++ src/App.vue
@@ -4,24 +4,38 @@ import L from 'leaflet'
 
 const lat = ref<number | null>(null);
 const long = ref<number | null>(null);
+const map = ref()
 const mapContainer = ref()
 
 const getLocation = async () => {
   if(navigator.geolocation) {
 
+    let marker: L.Marker | null = null;
+
     navigator.geolocation.watchPosition((location) => {
       lat.value = location.coords.latitude;
       long.value = location.coords.longitude;
+      map.value.setView([lat.value,long.value, 13]);
+
+      if(!marker) {
+        marker = L.marker([lat.value, long.value], {draggable: true})
+          .addTo(map.value)
+          .on("dragend", (event: L.DragEndEvent) => {
+            console.log(event)
+          });
+      } else {
+        marker.setLatLng([lat.value, long.value]);
+      }
     })
   }
 }
 
 onMounted(() => {
-  var map = L.map(mapContainer.value).setView([51.505, -0.09], 13);
+  map.value = L.map(mapContainer.value).setView([51.505, -0.09], 13);
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+}).addTo(map.value);
 })
 </script>
 
@@ -39,16 +53,5 @@ onMounted(() => {
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
